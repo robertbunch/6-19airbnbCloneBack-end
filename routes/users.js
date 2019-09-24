64dfa439684bd49a915c033dc34d201c5e96276c
Robert Bunch <rdbunch@gmail.com>
2019-09-24 14:31:02 -0400

    add login route

--- routes/users.js
+++ routes/users.js
@@ -54,7 +54,46 @@ router.post('/signup',(req, res, next)=>{
 })
 
 router.post('/login',(req, res)=>{
-  res.json(req.body);
+  const { email, password } = req.body;
+  // First: Check db for this email
+  const getUserQuery = `SELECT * FROM users WHERE email = ?`;
+  db.query(getUserQuery,[email],(err, results)=>{
+    if(err){throw err}
+    // check to see if there is a result
+    if(results.length > 0){
+      // found them!!!!
+      const thisRow = results[0];
+      // find out if the pass is correct
+      const isValidPass = bcrypt.compareSync(password,thisRow.password);
+      if(isValidPass){
+        // these are the droids we're looking for
+        const token = randToken.uid(50); // this is the users valet ticket
+        const updateUserTokenQuery = `UPDATE users
+          SET token = ? WHERE email = ?`
+        db.query(updateUserTokenQuery,[token,email],(err)=>{
+          if(err){throw err}
+        });
+
+        res.json({
+          msg: "loggedIn",
+          first: thisRow.first,
+          email: thisRow.email,
+          token
+        });
+      }else{
+        // lier lier, pants on fire
+        res.json({
+          msg: "badPass"
+        })
+      }
+
+    }else{
+      // no match
+      res.json({
+        msg: "noEmail"
+      })
+    }
+  })
 })
 
 module.exports = router;
