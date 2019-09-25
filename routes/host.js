f5b438f7147d1808452bcea71efe0d1af0b8c201
Robert Bunch <rdbunch@gmail.com>
2019-09-25 15:48:03 -0400

    Add multer

--- routes/host.js
+++ routes/host.js
@@ -1,10 +1,20 @@
 var express = require('express');
 var router = express.Router();
 const db = require('../db');
+var multer  = require('multer')
+var upload = multer({ dest: './public/images' })
+const fs = require('fs');
 
 //    post('/host/homes')
-router.post('/homes',(req, res)=>{
+router.post('/homes',upload.single('locationImage'),(req, res)=>{
     console.log(req.body);
+    console.log(req.file);
+
+    const f = req.file;
+    fs.rename(f.path, f.destination+'/'+f.originalname,(err)=>{
+        if(err) throw err;
+    })
+
     res.json(req.body);
 });
 
